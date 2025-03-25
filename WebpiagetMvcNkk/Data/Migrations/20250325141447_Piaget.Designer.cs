eef469530861c8a5fa874a11499d1ded304d6194
nicolasads261 <nicolasads261@gmail.com>
2025-03-25 11:19:20 -0300

    controles e migration database

--- WebpiagetMvcNkk/Data/Migrations/20250325141447_Piaget.Designer.cs
+++ WebpiagetMvcNkk/Data/Migrations/20250325141447_Piaget.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using WebpiagetMvcNkk.Data;
 
@@ -11,9 +12,11 @@ using WebpiagetMvcNkk.Data;
 namespace WebpiagetMvcNkk.Data.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20250325141447_Piaget")]
+    partial class Piaget
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
